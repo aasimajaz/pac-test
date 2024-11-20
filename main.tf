3878eee7051b45fa9c64f9fb38f2d04e8f1cea13
aasim <aasimajaz@gmail.com>
2024-11-20 09:54:01 +0300

    fixed untracked files

---
+++ main.tf
@@ -0,0 +1,70 @@
+#run "gcloud auth application-default login" to authenticate
+
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = "project-for-vpc-testing"
+  region = "me-central2"
+  zone   = "me-central2-a"
+}
+
+#creating a VPC network for testing
+resource "google_compute_network" "vpc_network" {
+  name = "cpg-network"
+  auto_create_subnetworks = false
+}
+
+
+#creating a private subnet to host servers with data and application
+resource "google_compute_subnetwork" "private-subnet" {
+  name          = "app-subnetwork"
+  ip_cidr_range = "10.10.100.0/24"
+  region        = "me-central2"
+  network       = google_compute_network.vpc_network.name
+  private_ip_google_access = "true"
+  }
+
+#creating a public subnet to host server with internet access and IP
+resource "google_compute_subnetwork" "public-subnet" {
+  name          = "web-subnetwork"
+  ip_cidr_range = "10.10.150.0/24"
+  region        = "me-central2"
+  network       = google_compute_network.vpc_network.name
+  private_ip_google_access = "true"
+  }
+
+
+# creating 3 vm instances
+resource "google_compute_instance" "app_instance" {
+ count = 3
+  name         = "app-instance-${count.index}"
+  machine_type = "e2-small"
+  zone         = "me-central2-a"
+  tags = ["type","app"]
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2204-lts"
+    }
+  }
+  network_interface {
+    subnetwork = google_compute_subnetwork.private-subnet.name
+  }
+}
+
+#creating a test bucket
+
+resource "google_storage_bucket" "example_bucket" {
+  name     = "cpg-example-bucket-1"
+  location = "me-central2"
+  force_destroy = true
+  project = "project-for-vpc-testing"
+
+  uniform_bucket_level_access = false
+}
